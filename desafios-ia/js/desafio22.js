97240730d693e535f4fd9449f8bf45b78ecc38b3
f4boco <ffabiano.olivr@gmail.com>
2026-08-04 13:10:12 -0300

    Finalizado (versão beta) do desafio 22

---
+++ desafios-ia/js/desafio22.js
@@ -0,0 +1,95 @@
+/**
+ * esse script é responsável por todas as partes do sistema.
+ * 
+ * autor: Fabiano O.
+ */
+
+// REFERÊNCIA AOS ELEMENTOS HTML
+const todosCompromissos = document.querySelector("#todosCompromissos");
+const passadosCompromissos = document.querySelector("#passadosCompromissos");
+const proxCompromisso = document.querySelector("#proxCompromisso");
+
+// VARIÁVES E ESTADOS
+const ESTADOS_SISTEMA = {
+    listaCompromissos: []
+}
+
+// INICIALIZAÇÃO
+iniciarEventos();
+
+// FUNÇÕES AUXILIÁRES
+function criarCompromissos() {
+    const hoje = new Date();
+    const compromissosGerados = [
+        {
+            descricao: "Reunião com Sócios Minoritários",
+            date: new Date(`2026-08-${(hoje.getDate() + 1).toString().padStart(2, "0")}T09:30:00-03:00`)
+        },
+        {
+            descricao: "Reunião para Pitch",
+            date: new Date(`2026-08-03T15:30:00-03:00`)
+        },
+        {
+            descricao: "Apresentação da Holding",
+            date: new Date(`2026-08-03T17:00:00-03:00`)
+        },
+        {
+            descricao: "Análise de rotina do Repositório",
+            date: new Date(`2026-08-${(hoje.getDate() + 3).toString().padStart(2, "0")}T08:30:00-03:00`)
+        },
+        {
+            descricao: "Apresentação da Holding",
+            date: new Date(`2026-07-${(hoje.getDate() + 4).toString().padStart(2, "0")}T15:30:00-03:00`)
+        }
+    ];
+    return compromissosGerados;
+}
+
+function montarSecao(compromissos, secao) {
+    return function () {
+        const secaoUpdate = secao;
+        secaoUpdate.innerHTML = "";
+
+        compromissos.forEach(compromisso => {
+            const cartaoCompromisso = document.createElement("div");
+            cartaoCompromisso.classList.add("cartao");
+            cartaoCompromisso.innerHTML = `
+                <p><strong>Compromisso:</strong> ${compromisso.descricao}</p>
+                <p><strong>Data:</strong> ${compromisso.date.toLocaleDateString("pt-BR")} 
+                    <strong>Horário: </strong> ${compromisso.date.toLocaleTimeString("pt-BR")}</p>
+            `;
+            secaoUpdate.appendChild(cartaoCompromisso);
+        });
+    }
+}
+
+// FUNÇÕES PRINCIPAIS
+function iniciarEventos() {
+    ESTADOS_SISTEMA.listaCompromissos = criarCompromissos();
+    listarCompromissos("todos", ESTADOS_SISTEMA.listaCompromissos, atualizarUI);
+    listarCompromissos("passado", ESTADOS_SISTEMA.listaCompromissos, atualizarUI);
+    listarCompromissos("prox", ESTADOS_SISTEMA.listaCompromissos, atualizarUI);
+}
+
+function listarCompromissos(periodoTemporal, compromissos, callback) {
+    let compromissosFiltrados = [];
+
+    if (periodoTemporal === "todos") {
+        compromissosFiltrados = compromissos.slice();
+    } else if (periodoTemporal === "passado") {
+        compromissosFiltrados = compromissos.filter(comp => comp.date < new Date());
+    } else {
+        compromissosFiltrados = compromissos.filter(comp => comp.date >= new Date()).slice(0, 1);
+    }
+    callback(compromissosFiltrados, periodoTemporal);
+}
+
+function atualizarUI(compromissos, periodoTemporal) {
+    if (periodoTemporal === "todos") {
+        montarSecao(compromissos, todosCompromissos)();
+    } else if (periodoTemporal === "passado") {
+        montarSecao(compromissos, passadosCompromissos)();
+    } else {
+        montarSecao(compromissos, proxCompromisso)();
+    }
+}

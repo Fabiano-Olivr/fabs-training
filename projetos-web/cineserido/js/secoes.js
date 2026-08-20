02cf0ee3612766c09f75f597684471faafc983f2
f4boco <ffabiano.olivr@gmail.com>
2026-08-20 05:48:56 -0300

    Finaliza, parcialmente, o js da página de 'seções'

--- projetos-web/cineserido/js/secoes.js
+++ projetos-web/cineserido/js/secoes.js
@@ -8,9 +8,9 @@
 import { recuperarDados } from "./storage.js";
 
 // REFERÊNCIAS AOS ELEMENTOS HTML
-const formularioFiltroSecoes = document.querySelector("formulario-filtro-secoes");
+const formularioFiltroSecoes = document.querySelector("#formulario-filtro-secoes");
 const inNomeFilme = document.querySelector("#filtro-filme");
-const inDataSecao = document.querySelector("filtro-data");
+const inDataSecao = document.querySelector("#filtro-data");
 const inOutSelectSala = document.querySelector("#filtro-sala");
 const inComVagas = document.querySelector("#filtro-disponivel");
 const conteinerSessoes = document.querySelector("#conteiner-sessoes");
@@ -28,6 +28,11 @@ function renderizarOpcoesDeSalas(listaSecoes) {
     });
 }
 
+function buscarNome(nomeBuscado, nome) {
+    const regEx = new RegExp(nomeBuscado, "gi");
+    return regEx.test(nome);
+}
+
 // FUNÇÕES PRINCIPAIS
 function inicializarEventos() {
     const secoes = recuperarDados("secoes");
@@ -38,19 +43,16 @@ function inicializarEventos() {
 function renderizarSecoes(listaSecoes) {
     conteinerSessoes.innerHTML = "";
 
-    if (listaSecoes.length === 0) {
-        conteinerSessoes.innerHTML = `
-            <p>Nenhuma Seção encontrada</p>
-        `;
-    } else {
+    if (listaSecoes) {
         listaSecoes.forEach(secao => {
             const cartaoSecao = document.createElement("article");
             cartaoSecao.className = "card-sessao";
 
+            const data = new Date(`${secao.data}T${secao.horario}`).toLocaleDateString();
             cartaoSecao.innerHTML = `
                 <h3>${secao.filme.nome}</h3>
-                <p><strong>Sala:</strong> ${secao.sala.padStart(2, "0")}</p>
-                <p><strong>Data:</strong> ${secao.data}</p>
+                <p><strong>Sala:</strong> ${secao.sala}</p>
+                <p><strong>Data:</strong> ${data}</p>
                 <p><strong>Horário:</strong> ${secao.horario}</p>
                 <p><strong>Vagas:</strong> 25/50</p>
                 <a href="reserva.html" class="botao-sucesso">Reservar Assento</a>
@@ -58,10 +60,41 @@ function renderizarSecoes(listaSecoes) {
 
             conteinerSessoes.appendChild(cartaoSecao);
         });
+    } else {
+        conteinerSessoes.innerHTML = `
+            <p>Nenhuma seção encontrada</p>
+        `;
     }
 }
 
+function filtrarSecoes(listaSecoes, callback) {
+    const nomeFilme = inNomeFilme.value.trim();
+    const dataSecao = inDataSecao.value;
+    const numeroDaSala = Number(inOutSelectSala.value);
+    const comVagas = inComVagas.checked;
+
+    const secoesEncontradas = [];
+    for (const secao of listaSecoes) {
+        if (!buscarNome(nomeFilme, secao.filme.nome))
+            continue;
+        if (dataSecao !== "" && dataSecao !== secao.data)
+            continue;
+        // if (comVagas)
+        // Fazer qtdIngressos - capacidadeSala > 0?
+        if (numeroDaSala > 0 && numeroDaSala !== Number(secao.sala))
+            continue;
+
+        secoesEncontradas.push(secao);
+    }
+
+    callback(secoesEncontradas);
+}
+
 // EVENTOS
+formularioFiltroSecoes.addEventListener("submit", function (e) {
+    e.preventDefault();
+    filtrarSecoes(recuperarDados("secoes"), renderizarSecoes);
+});
 
 // INICIALIZAÇÃO
 inicializarEventos();

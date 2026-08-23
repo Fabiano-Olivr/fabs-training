3adf353fce26c6b264771b136eda93077a32e5bf
f4boco <ffabiano.olivr@gmail.com>
2026-08-23 15:21:52 -0300

    Iniciado o desenvolvimento da função de reservar ingressos

--- projetos-web/cineserido/js/reserva.js
+++ projetos-web/cineserido/js/reserva.js
@@ -11,8 +11,18 @@ import { recuperarDados, salvarDados } from "./storage.js";
 const outNomeFilme = document.querySelector("#nome-filme");
 const outSalaSecao = document.querySelector("#numero-sala");
 const outHorarioSecao = document.querySelector("#horario-secao");
+const formularioReserva = document.querySelector("#form-reserva");
+const inNomeCliente = document.querySelector("#nome-cliente");
+const outTextoSelecionados = document.querySelector("#texto-assentos");
+const outValorTotal = document.querySelector("#texto-valor-total");
+const outMapaAssentos = document.querySelector("#mapa-assentos");
 
 // FUNÇÕES AUXILIÁRES
+function verificarNomeCliente(nome) {
+    const regEx = /^[a-zA-z]{3,} [a-zA-Z]{2,}/i;
+    return regEx.test(nome);
+}
+
 function renderizarInformacoesSecao(secao) {
     const dataSecaoFormat = new Date(`${secao.data}T${secao.horario}`).toLocaleDateString();
 
@@ -33,6 +43,38 @@ function buscarSecaoSelecionada() {
 
     return todasAsSecoes.find(secao => secao.idSecao === idSecaoSelecionada.secaoIdTemp);
 }
+
+function reservarIngressos() {
+    const informacoesSecao = buscarSecaoSelecionada();
+    const nomeCliente = inNomeCliente.value.trim();
+
+    if (!verificarNomeCliente(nomeCliente)) {
+        alert("Informe um Nome Completo!");
+        inNomeCliente.focus();
+        return;
+    }
+
+    // retorna uma NodeList contendo todos os botões de assentos selecionados
+    const nodeAssentosSelecionados = document.querySelectorAll("#mapa-assentos .assento.selecionado");
+    const assentosSelecionados = [...nodeAssentosSelecionados]; // converte a nodeList para vetor
+    const qtdAssentosSelecionado = assentosSelecionados.length;
+
+    if (qtdAssentosSelecionado < 1) {
+        console.log("Selecione, pelo menos, um assento!");
+        return;
+    }
+
+    const textoAssentosSelecionados = assentosSelecionados.map(assento => assento.innerText).join(", ");
+    const valorTotal = qtdAssentosSelecionado * Number(informacoesSecao.precoIngresso);
+    
+    /* Modificar o storage para adicionar a funcionalidade de acessar um objeto e modificar um atributo dele */
+}
+
+// EVENTOS
+formularioReserva.addEventListener("submit", function (e) {
+    e.preventDefault();
+    reservarIngressos();
+});
 
 // INICIALIZAÇÃO
 inicializarEventos();
